461fc3409b361c26c823ae093a8f8e00e657d008
Martial Saunois <martial.saunois@aramisauto.com>
2015-02-13 10:08:26 +0100

    Add new config parameter "project_root".

--- config/app.php
+++ config/app.php
@@ -3,6 +3,7 @@
 define('CONFIG_PROJECT_ROOT', __DIR__ . '/..');
 
 return [
+    'project_root' => CONFIG_PROJECT_ROOT,
     'assets' => [
         'source_paths' => [
             CONFIG_PROJECT_ROOT . '/src/Front/View/Home/js'
