67063c3b7fc8253a2230a4cf83484f92cb7c43e4
Martial Saunois <saunois.martial@gmail.com>
2015-03-09 13:08:22 +0100

    Add unit tests for the Profile type.

---
+++ tests/Form/ProfileTest.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace Martial\Warez\Tests\Form;
+
+use Martial\Warez\Form\Profile;
+
+class ProfileTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @var Profile
+     */
+    public $form;
+
+    public function testBuildForm()
+    {
+        $formBuilder = $this->getMock('\Symfony\Component\Form\FormBuilderInterface');
+
+        $formBuilder
+            ->expects($this->once())
+            ->method('add')
+            ->with($this->equalTo('trackerPassword'), $this->equalTo('password'))
+            ->will($this->returnValue($formBuilder));
+
+        $this->form->buildForm($formBuilder, []);
+    }
+
+    public function testDefaultOptions()
+    {
+        $resolver = $this->getMock('\Symfony\Component\OptionsResolver\OptionsResolverInterface');
+
+        $resolver
+            ->expects($this->once())
+            ->method('setDefaults')
+            ->with($this->equalTo(['data_class' => '\Martial\Warez\User\Entity\Profile']))
+            ->will($this->returnValue($resolver));
+
+        $this->form->setDefaultOptions($resolver);
+    }
+
+    public function testGetName()
+    {
+        $this->assertSame('profile', $this->form->getName());
+    }
+
+    protected function setUp()
+    {
+        $this->form = new Profile();
+    }
+}
