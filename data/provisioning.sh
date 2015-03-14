19954bf0107ad31f06e3b9caa97dcfbb41156d94
Martial Saunois <saunois.martial@gmail.com>
2015-03-14 22:17:46 +0100

    Create a Vagrantfile and a provisioning script.

---
+++ data/provisioning.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+sudo usermod -aG adm vagrant
+sudo apt-get update && sudo apt-get upgrade
+sudo apt-get install -y php5-common php5-fpm php5-curl php5-json php5-mysqlnd php5-xsl php5-memcached php5-intl \
+php5-mcrypt php5-xdebug nginx transmission-daemon memcached mysql-server
+
+cd /etc/nginx/sites-available
+sudo cp /vagrant/data/nginx-vhost warez
+cd ../sites-enabled
+sudo ln -s ../sites-available/warez
+sudo service nginx restart
