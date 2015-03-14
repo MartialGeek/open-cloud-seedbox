1d88f5f661cc292c45304e2de96cc15b2fc39fb0
Martial Saunois <saunois.martial@gmail.com>
2015-03-14 22:33:07 +0100

    Add php5-cli to the provisioning.

--- data/provisioning.sh
+++ data/provisioning.sh
@@ -2,7 +2,7 @@
 
 sudo usermod -aG adm vagrant
 sudo apt-get update && sudo apt-get upgrade
-sudo apt-get install -y php5-common php5-fpm php5-curl php5-json php5-mysqlnd php5-xsl php5-memcached php5-intl \
+sudo apt-get install -y php5-common php5-fpm php5-cli php5-curl php5-json php5-mysqlnd php5-xsl php5-memcached php5-intl \
 php5-mcrypt php5-xdebug nginx transmission-daemon memcached mysql-server
 
 cd /etc/nginx/sites-available
