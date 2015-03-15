92ff9674fe466422a07090076bf7f89ee36ef265
Martial Saunois <saunois.martial@gmail.com>
2015-03-15 14:02:06 +0100

    Add curl in the provisioning script.

--- data/provisioning.sh
+++ data/provisioning.sh
@@ -2,8 +2,8 @@
 
 sudo usermod -aG adm vagrant
 sudo apt-get update && sudo apt-get upgrade
-sudo apt-get install -y php5-common php5-fpm php5-cli php5-curl php5-json php5-mysqlnd php5-xsl php5-memcached php5-intl \
-php5-mcrypt php5-xdebug nginx transmission-daemon memcached mysql-server
+sudo apt-get install -y php5-common php5-fpm php5-cli php5-curl php5-json php5-mysqlnd php5-xsl php5-memcached \
+php5-intl php5-mcrypt php5-xdebug nginx transmission-daemon memcached mysql-server curl
 
 cd /etc/nginx/sites-available
 sudo cp /vagrant/data/nginx-vhost warez
