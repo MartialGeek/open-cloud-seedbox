33f645c2cf7e6863707e3513bc1c4137d78c3b5f
Martial Saunois <martial.saunois@aramisauto.com>
2015-03-20 13:58:41 +0100

    Removing the PHP Puppet module, all packages are now installed by hand.

--- data/provisioning/manifests/default.pp
+++ data/provisioning/manifests/default.pp
@@ -1,22 +1,51 @@
+Exec['apt_update'] -> Package <| |>
+
 include '::mysql::server'
-include php
 include nginx
 
 $root = '/var/www/warez/web'
 
-class { [
-    'php::fpm',
-    'php::cli',
-    'php::extension::curl',
-    'php::extension::intl',
-    'php::extension::mcrypt',
-    'php::extension::memcached',
-    'php::extension::mysql',
-    'php::extension::opcache',
-    'php::extension::xdebug',
-    'php::composer',
-    'php::composer::auto_update'
-]: }
+$php_packages = [
+  'php5-common',
+  'php5-cli',
+  'php5-fpm',
+  'php5-curl',
+  'php5-intl',
+  'php5-json',
+  'php5-mcrypt',
+  'php5-memcached',
+  'php5-mysqlnd',
+  'php5-xdebug',
+  'php5-xsl',
+]
+
+$misc_packages = [
+    'transmission-daemon',
+    'memcached',
+    'curl'
+]
+
+$env_path_line = 'PATH=$HOME/.composer/vendor/bin:$PATH'
+$profile_bash_script = '/home/vagrant/.profile'
+
+package { $php_packages:
+    ensure  => installed,
+    notify  => Service['nginx']
+}
+
+exec { 'install_composer':
+    command => '/usr/bin/curl -sS https://getcomposer.org/installer | sudo /usr/bin/php -- --install-dir=/usr/local/bin --filename=composer',
+    require => Package['php5-cli'],
+    unless => '/bin/ls /usr/local/bin/composer'
+}
+
+file { '/home/vagrant/.profile':
+    ensure => present
+} ->
+exec { 'add_composer_path':
+    command => "/bin/echo '${env_path_line}' >> ${$profile_bash_script}",
+    unless => "/bin/grep '${$env_path_line}' ${$profile_bash_script}"
+}
 
 nginx::resource::vhost { 'warez.dev':
     www_root    => $root,
@@ -34,11 +63,6 @@ nginx::resource::location { 'warez_php':
     }
 }
 
-package { 'transmission-daemon':
+package { $misc_packages:
     ensure => installed
 }
-
-package { 'memcached':
-    ensure => installed
-}
-
