f37799ed2932d0ff19955ce4ceeab176a0ee722e
Martial Saunois <martial.saunois@aramisauto.com>
2015-04-07 17:39:48 +0200

    Clean the full qualified path in the provisioning.

--- data/provisioning/manifests/default.pp
+++ data/provisioning/manifests/default.pp
@@ -38,10 +38,10 @@ package { $php_packages:
 }
 
 exec { 'download_nodejs':
-    command     => '/usr/bin/curl http://nodejs.org/dist/v0.12.0/node-v0.12.0-linux-x64.tar.gz -o /tmp/nodejs.tar.gz',
+    command     => 'curl http://nodejs.org/dist/v0.12.0/node-v0.12.0-linux-x64.tar.gz -o /tmp/nodejs.tar.gz',
     require     => Package['curl'],
     notify      => Exec['untar_nodejs'],
-    unless      => '/usr/bin/test -f /opt/nodejs/bin/node'
+    unless      => 'test -f /opt/nodejs/bin/node'
 }
 
 file { '/opt/nodejs':
@@ -49,7 +49,7 @@ file { '/opt/nodejs':
 }
 
 exec { 'untar_nodejs':
-    command     => '/bin/tar xvf /tmp/nodejs.tar.gz --strip-components 1',
+    command     => 'tar xvf /tmp/nodejs.tar.gz --strip-components 1',
     cwd         => '/opt/nodejs',
     require     => File['/opt/nodejs'],
     refreshonly => true
@@ -61,20 +61,20 @@ file { '/tmp/nodejs.tar.gz':
 }
 
 exec { 'install_sass':
-    command => '/usr/bin/gem install sass',
-    unless  => '/usr/bin/which sass',
+    command => 'gem install sass',
+    unless  => 'which sass',
 }
 
 exec { 'install_grunt':
-    command => '/opt/nodejs/bin/npm install -g grunt-cli',
+    command => 'npm install -g grunt-cli',
     require => Exec['untar_nodejs'],
-    unless => '/opt/nodejs/bin/npm list -g | /bin/grep grunt-cli'
+    unless => 'npm list -g | grep grunt-cli'
 }
 
 exec { 'install_bower':
-    command => '/opt/nodejs/bin/npm install -g bower',
+    command => 'npm install -g bower',
     require => Exec['untar_nodejs'],
-    unless => '/opt/nodejs/bin/npm list -g | /bin/grep bower'
+    unless => 'npm list -g | grep bower'
 }
 
 file { '/var/log/php5-fpm.log':
@@ -85,17 +85,17 @@ file { '/var/log/php5-fpm.log':
 }
 
 exec { 'install_composer':
-    command => '/usr/bin/curl -sS https://getcomposer.org/installer | sudo /usr/bin/php -- --install-dir=/usr/local/bin --filename=composer',
+    command => 'curl -sS https://getcomposer.org/installer | sudo php -- --install-dir=/usr/local/bin --filename=composer',
     require => Package['php5-cli'],
-    unless => '/bin/ls /usr/local/bin/composer'
+    unless => 'ls /usr/local/bin/composer'
 }
 
 file { '/home/vagrant/.profile':
     ensure => present
 } ->
 exec { 'add_composer_path':
-    command => "/bin/echo '${env_path_line}' >> ${$profile_bash_script}",
-    unless => "/bin/grep '${$env_path_line}' ${$profile_bash_script}"
+    command => "echo '${env_path_line}' >> ${$profile_bash_script}",
+    unless => "grep '${$env_path_line}' ${$profile_bash_script}"
 }
 
 nginx::resource::vhost { 'warez.dev':
@@ -170,24 +170,35 @@ file { '/etc/transmission-daemon/settings.json':
     require => File['/home/vagrant/.composer/auth.json']
 }
 
+file { '/home/vagrant/.composer':
+    ensure => directory,
+    owner  => 'vagrant',
+    group  => 'vagrant',
+    mode   => 0775
+}
+
 file { '/home/vagrant/.composer/auth.json':
     ensure  => present,
     content => file('/var/www/warez/data/provisioning/files/composer-auth.json'),
     owner   => 'vagrant',
     group   => 'vagrant',
-    mode    => 0600
+    mode    => 0600,
+    require => File['/home/vagrant/.composer']
 }
 
 exec { 'install_composer_dependencies':
-    command     => '/usr/local/bin/composer install',
-    require     => Exec['install_composer'],
+    command     => 'composer install',
+    require     => [
+        Exec['install_composer'],
+        File['/home/vagrant/.composer/auth.json']
+    ],
     cwd         => $project_path,
     environment => 'HOME=/home/vagrant',
     user        => 'vagrant'
 }
 
 exec { 'install_npm_dependencies':
-    command => '/opt/nodejs/bin/npm install',
+    command => 'npm install',
     require => Exec['untar_nodejs'],
     cwd     => $project_path,
     user    => 'vagrant',
@@ -195,7 +206,7 @@ exec { 'install_npm_dependencies':
 }
 
 exec { 'install_bower_dependencies':
-    command     => '/opt/nodejs/bin/bower install --config.interactive=false',
+    command     => 'bower install --config.interactive=false',
     require     => Exec['install_bower'],
     cwd         => $project_path,
     environment => 'HOME=/home/vagrant',
@@ -203,7 +214,7 @@ exec { 'install_bower_dependencies':
 }
 
 exec { 'build_assets':
-    command => '/opt/nodejs/bin/grunt build',
+    command => 'grunt build',
     require => [
         Exec['install_grunt'],
         Exec['install_npm_dependencies']
