08082dcc081fb538b854fa8c435402f75f678020
Martial Saunois <saunois.martial@gmail.com>
2015-05-29 14:28:33 +0200

    Log the upload listener actions.

--- src/Application/Bootstrap.php
+++ src/Application/Bootstrap.php
@@ -323,8 +323,11 @@ class Bootstrap
             return $consumer;
         });
 
-        $app['upload.listener'] = $app->share(function() {
-            return new UploadListener();
+        $app['upload.listener'] = $app->share(function() use ($app) {
+            $listener = new UploadListener();
+            $listener->setLogger($app['logger']);
+
+            return $listener;
         });
     }
 
--- src/Upload/UploadListener.php
+++ src/Upload/UploadListener.php
@@ -3,10 +3,24 @@
 namespace Martial\Warez\Upload;
 
 use Martial\Warez\Upload\Freebox\FreeboxManager;
+use Psr\Log\LoggerInterface;
 use Symfony\Component\HttpKernel\Event\PostResponseEvent;
 
 class UploadListener
 {
+    /**
+     * @var LoggerInterface
+     */
+    private $logger;
+
+    /**
+     * @param LoggerInterface $logger
+     */
+    public function setLogger(LoggerInterface $logger)
+    {
+        $this->logger = $logger;
+    }
+
     /**
      * Deletes the generated archives after they are downloaded.
      *
@@ -24,6 +38,7 @@ class UploadListener
         $uploadType = $request->query->get('upload-type');
 
         if ($uploadType == FreeboxManager::UPLOAD_TYPE_ARCHIVE) {
+            $this->logger->info('An archive must be removed.');
             $file = $request->query->get('filename');
             unlink($file);
         }
