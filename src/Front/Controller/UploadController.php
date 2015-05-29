01468032e600c22633504f73aa12931e5e884cb2
Martial Saunois <saunois.martial@gmail.com>
2015-05-29 15:27:39 +0200

    Remove unused "use" statements.

--- src/Front/Controller/UploadController.php
+++ src/Front/Controller/UploadController.php
@@ -2,11 +2,9 @@
 
 namespace Martial\Warez\Front\Controller;
 
-use Martial\Warez\Upload\Freebox\FreeboxManager;
 use Symfony\Component\HttpFoundation\BinaryFileResponse;
 use Symfony\Component\HttpFoundation\File\File;
 use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\ResponseHeaderBag;
 
 class UploadController
