24567455cede44c3eb720a5e8a592e6bd4f56644
Martial Saunois <saunois.martial@gmail.com>
2015-06-10 17:05:49 +0200

    Add unit tests for the TrackerSettingsEntity.

---
+++ tests/Settings/Entity/TrackerSettingsEntityTest.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace Martial\Warez\Tests\Settings\Entity;
+
+use Martial\Warez\Settings\Entity\TrackerSettingsEntity;
+use Martial\Warez\User\Entity\User;
+
+class TrackerSettingsEntityTest extends \PHPUnit_Framework_TestCase
+{
+    public function testEntity()
+    {
+        $id = 42;
+        $password = password_hash('password', PASSWORD_BCRYPT);
+        $username = 'joe';
+        $user = new User();
+        $createdAt = new \DateTime('-1 year');
+        $updatedAt = new \DateTime();
+
+        $settings = new TrackerSettingsEntity();
+        $settings
+            ->setId($id)
+            ->setPassword($password)
+            ->setUsername($username)
+            ->setUser($user)
+            ->setCreatedAt($createdAt)
+            ->setUpdatedAt($updatedAt);
+
+        $this->assertSame($id, $settings->getId());
+        $this->assertSame($password, $settings->getPassword());
+        $this->assertSame($username, $settings->getUsername());
+        $this->assertSame($user, $settings->getUser());
+        $this->assertSame($createdAt, $settings->getCreatedAt());
+        $this->assertSame($updatedAt, $settings->getUpdatedAt());
+    }
+
+    public function testOnCreateHook()
+    {
+        $settings = new TrackerSettingsEntity();
+
+        $this->assertNull($settings->getCreatedAt());
+        $this->assertNull($settings->getUpdatedAt());
+
+        $settings->onCreate();
+
+        $this->assertInstanceOf('\DateTime', $settings->getCreatedAt());
+        $this->assertInstanceOf('\DateTime', $settings->getUpdatedAt());
+    }
+
+    public function testOnUpdateHook()
+    {
+        $settings = new TrackerSettingsEntity();
+
+        $this->assertNull($settings->getUpdatedAt());
+        $settings->onUpdate();
+        $this->assertInstanceOf('\DateTime', $settings->getUpdatedAt());
+    }
+}
