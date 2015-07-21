373f4d3f2727a6c4b00d7f7875ee7f9f86332572
Martial Saunois <saunois.martial@gmail.com>
2015-07-21 17:44:00 +0200

    Add the ability to sort the files by name (ascending and descending).

--- src/Front/View/Home/js/filesystem/file-browser.js
+++ src/Front/View/Home/js/filesystem/file-browser.js
@@ -28,12 +28,15 @@ $(function() {
         return path == '' ? '/' : path.substr(1);
     };
 
+    var items = [];
+
     var browse = function(path) {
         var uri = getBrowsePathUri(path);
         var currentPath = getCurrentPath();
 
         $
             .getJSON(uri, function(res) {
+                items = res.items;
                 buildFileTab(res);
                 window.history.pushState({'html': res, 'pageTitle': path}, path, uri);
             })
@@ -46,73 +49,109 @@ $(function() {
             });
     };
 
+    var sortItemsAlphabetically = function(items, direction) {
+        direction = direction || 'asc';
+
+        items.sort(function(a, b) {
+            var first = direction == 'asc' ? a : b;
+            var second = direction == 'asc' ? b : a;
+
+            if (first.filename > second.filename) {
+                return 1;
+            }
+
+            if (first.filename < second.filename) {
+                return -1;
+            }
+
+            return 0;
+        });
+    };
+
+    var buildLink = function(linkPath, relativePath, text) {
+        return '<a href="' + linkPath + '" data-file-type="directory" data-file-path="' +
+            relativePath + '">' + text + '</a>';
+    };
+
+    var buildRows = function(items, options) {
+        options = options || {
+                sortOrder: 'asc',
+                sortType: 'alpha'
+            };
+
+        var output = '';
+        var directories = [];
+        var files = [];
+
+        var extractDirectoriesAndFiles = (function(items, directories, files) {
+            for (var i = 0; i < items.length; i++) {
+                if (items[i].isDir) {
+                    directories.push(items[i]);
+                } else {
+                    files.push(items[i]);
+                }
+            }
+        });
+
+        extractDirectoriesAndFiles(items, directories, files);
+
+        if (options.sortType == 'alpha') {
+            sortItemsAlphabetically(directories, options.sortOrder);
+            sortItemsAlphabetically(files, options.sortOrder);
+        }
+
+        var buildRow = (function(items) {
+            var output = '';
+
+            for (var i = 0; i < items.length; i++) {
+                if (items[i].filename.charAt(0) == '.') {
+                    return;
+                }
+
+                output+= '<tr><td>';
+
+                if (items[i].isDir) {
+                    output += buildLink(
+                        getBrowsePathUri(items[i].relativePath),
+                        items[i].relativePath,
+                        items[i].filename
+                    );
+                } else {
+                    output += items[i].filename;
+                }
+
+                output += '</td><td>';
+
+                if (!items[i].isDir) {
+                    output += '<a href="' + getUploadUri(items[i].fullPath) + '">Download</a>'
+                } else {
+                    output += '&nbsp;'
+                }
+
+                output += '</td>';
+            }
+
+            return output;
+        });
+
+        output += buildRow(directories);
+        output += buildRow(files);
+
+        return output;
+    };
+
     var tableBody = $('#file-browser-tab').find('tbody');
 
     var buildFileTab = function(res) {
         var html = '';
 
-        var buildLink = function(linkPath, relativePath, text) {
-            return '<a href="' + linkPath + '" data-file-type="directory" data-file-path="' +
-                relativePath + '">' + text + '</a>';
-        };
-
         if (res.path != '/') {
-            html += '<tr><td>' + buildLink(getBrowsePathUri(res.parentPath), res.parentPath, '<-- Parent') +
+            html += '<tr id="parent-link"><td>' +
+                buildLink(getBrowsePathUri(res.parentPath), res.parentPath, '<-- Parent') +
                 '</td><td>&nbsp;</td></tr>';
         }
 
-        var directories = [];
-        var files = [];
-
-        res.items.forEach(function(item) {
-            if (item.isDir) {
-                directories.push(item);
-            } else {
-                files.push(item);
-            }
-        });
-
-        var sortItemsAlphabetically = function(a, b) {
-            if (a.filename > b.filename) {
-                return 1;
-            }
-
-            if (a.filename < b.filename) {
-                return -1;
-            }
-
-            return 0;
-        };
-
-        directories.sort(sortItemsAlphabetically);
-        files.sort(sortItemsAlphabetically);
-
-        var buildRows = function(item) {
-            if (item.filename.charAt(0) == '.') {
-                return;
-            }
-
-            html+= '<tr><td>';
-
-            if (item.isDir) {
-                html += buildLink(getBrowsePathUri(item.relativePath), item.relativePath, item.filename);
-            } else {
-                html += item.filename;
-            }
-
-            html += '</td><td>';
-
-            if (!item.isDir) {
-                html += '<a href="' + getUploadUri(item.fullPath) + '">Download</a>'
-            } else {
-                html += '&nbsp;'
-            }
-
-            html += '</td>';
-        };
-
-        directories.forEach(buildRows);
-        files.forEach(buildRows);
+        html += buildRows(res.items);
 
         tableBody.html(html);
     };
@@ -123,6 +162,21 @@ $(function() {
         event.preventDefault();
         tableBody.html('');
         browse(path);
+    });
+
+    var sortContext = {
+        sortType: 'alpha',
+        sortOrder: 'asc'
+    };
+
+    $('#sort-by-filename').on('click', function() {
+        var parentLink = $('#parent-link');
+        var currentSortOrder = sortContext.sortOrder;
+
+        tableBody.html('');
+        tableBody.append(parentLink);
+        sortContext.sortOrder = currentSortOrder == 'asc' ? 'desc' : 'asc';
+        tableBody.append(buildRows(items, sortContext));
     });
 
     browse(getCurrentPath());
