de524356748c6f3e5fbaaf2115ca059c353dd40e
Martial Saunois <martial.saunois@skores.com>
2017-04-19 15:11:47 +0200

    Create the docker-compose.yml

---
+++ data/structure.sql
@@ -0,0 +1,43 @@
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `username` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
+  `email` varchar(250) COLLATE utf8_unicode_ci NOT NULL,
+  `password` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `created_at` datetime NOT NULL,
+  `updated_at` datetime NOT NULL,
+  `cookie_token_id` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `cookie_token_hash` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `UNIQ_1483A5E9F85E0677` (`username`),
+  UNIQUE KEY `UNIQ_1483A5E9E7927C74` (`email`),
+  UNIQUE KEY `UNIQ_1483A5E9BBDA0131` (`cookie_token_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+CREATE TABLE `settings_tracker` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `user_id` int(11) DEFAULT NULL,
+  `tracker_password` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `tracker_username` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `created_at` datetime NOT NULL,
+  `updated_at` datetime NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `UNIQ_1267829FA76ED395` (`user_id`),
+  CONSTRAINT `FK_1267829FA76ED395` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+CREATE TABLE `settings_freebox` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `user_id` int(11) DEFAULT NULL,
+  `transport_host` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `transport_port` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `app_id` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `app_name` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `app_version` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `device_name` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `session_token` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `app_token` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `challenge` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `UNIQ_2726CC7FA76ED395` (`user_id`),
+  CONSTRAINT `FK_2726CC7FA76ED395` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
